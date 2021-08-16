f917509b4f305af9bd486a2486975d72b9e1f6fb
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-08-16 18:27:03 +0600

    посты выводятся, отправляю

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import axios from "axios";
 import './App.css';
 
 function App() {
+    const url = 'http://146.185.154.90:8000/messages';
+    let interval = null
+
     const [posts, setPosts] = useState([]);
-    const [page, setPage] = useState(1);
     const [authorInput, setAuthorInput] = useState("");
     const [textInput, setTextInput] = useState("");
 
@@ -18,17 +20,54 @@ function App() {
         setTextInput(value);
     };
 
-    useEffect(() => {
-        let lastDate = '';
+    const getData = async () => {
+        try {
+            let data = null
+            console.log('posts', posts)
+            if (posts.length > 0) {
+                console.log('> 0')
+                data = await axios.get(url + '?datetime=' + posts[posts.length - 1]['datetime'])
+            } else {
+                data = await axios.get(url)
+            }
+            console.log(data)
+            if (data.data.length > 0) {
+                setPosts((prev) => {
+                    return [...prev].concat(data.data);
+                })
+                clearInterval(interval)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
-    }, [page]);
+    useEffect(() => {
+        interval = setInterval(async () => {
+            await getData()
+        }, 3000);
+    }, [posts]);
+
+    console.log('state' + posts[0])
+
 
     return (
         <div className="container">
             <div className="inner-container">
-                <InputForm add={() => addPost}/>
+                <InputForm
+                    setText={() => changeTextInputValue()}
+                    setAuthor={() => changeAuthorInputValue()}
+                    // add={() => addPost()}
+                />
                 {posts.map((post, index) => {
-                    return <Post key={index} author={post.author} date={post.date} text={post.data}/>
+                    return (
+                        <Post
+                            key={index}
+                            author={post.author}
+                            date={post.date}
+                            text={post.text}
+                        />
+                    )
                 })}
             </div>
         </div>
